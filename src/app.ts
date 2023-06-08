ba69e443e0902a68c93f93ab60c56cd4121539de
BayajidAlam <bayajidalam2001@gmail.com>
2023-06-08 12:03:57 +0600

    semester controller, route added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import { UserRoutes } from './app/modules/user/user.route';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 
 const app: Application = express();
 
@@ -11,8 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// application route
+// application routes
 app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 
 // testing
 // app.get('/okk', async (req: Request, res: Response, next: NextFunction) => {

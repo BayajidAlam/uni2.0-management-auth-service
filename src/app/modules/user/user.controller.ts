6726bf6a5a9fc2d0b3b7e8a950a1968d7e473d37
BayajidAlam <bayajidalam2001@gmail.com>
2023-06-09 21:05:50 +0600

    optimized application route and controllers

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,17 @@
+import { Request, Response } from 'express';
 import { UserService } from './user.service';
-import { RequestHandler } from 'express';
+import catchAsync from '../../../shared/catchAsync';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
-    const { user } = req.body;
-    const result = await UserService.createUser(user);
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { user } = req.body;
+  const result = await UserService.createUser(user);
 
-    res.status(200).json({
-      success: true,
-      message: 'User created successfully!',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: 'User created successfully!',
+    data: result,
+  });
+});
 
 export const UserController = {
   createUser,

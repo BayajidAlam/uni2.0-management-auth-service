6fe2b3f8dd876aa00fc7f6419266097eba0008b1
BayajidAlam <bayajidalam2001@gmail.com>
2023-06-09 19:50:24 +0600

    Semester Code Validation in service layer01 created

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,15 @@
 import { AcademicSemester } from './academicSemester.model';
 import { IAcademicSemester } from './academicSemester.interface';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
 
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid semester code');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
